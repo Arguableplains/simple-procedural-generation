b7beddeeb0fcec17d890e06a0e62039e78b0a4af
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2025-01-31 22:56:29 -0300

    First time on rendering random angled rods that grow in number by passing time

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,18 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <random>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
     // Constants
-    default_random_engine generator((unsigned)time(0));
-    uniform_real_distribution<float> distribution(-90.0f, 90.0f);
+    default_random_engine generator(time(0));
+    uniform_real_distribution<float> distribution(180.0f, 0.0f);
     double new_time;
     float new_angle = 0.0f;
+    int counter = 0;
 
     if (!glfwInit())
         return -1;
@@ -33,18 +35,21 @@ int main(){
 
     // Elements vertices and indices to draw
 
-    float vertices[] = {
+    vector<float> vertices = {
         0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-        0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f
+        0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f
     };
 
-    unsigned int indices[] = {
+    vector <unsigned int> indices = {
         0, 1
     };
 
     // Main GL objects
     Shaders shader_obj;
     ElementsMath elementsmath;
+
+    float last_endpoint[2] = {vertices[6], vertices[7]};
+    float new_endpoint[2];
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)){
@@ -57,16 +62,48 @@ int main(){
         glClear(GL_COLOR_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST | GL_DEPTH_BUFFER_BIT);
 
-        // Rotate Tick
+        // Timer Tick
         if((glfwGetTime() - new_time) > 1.0){
-            new_angle = distribution(generator);
+            counter+=2;
+            new_angle = (distribution(generator)/180)*acos(-1);
             new_time = glfwGetTime();
+
+            new_endpoint[0] = last_endpoint[0] + 0.5f * cos(new_angle);
+            new_endpoint[1] = last_endpoint[1] + 0.5f * sin(new_angle);
+
+            // Adding new informations to the main array - new rods
+            vertices.push_back(last_endpoint[0]);
+            vertices.push_back(last_endpoint[1]);
+            vertices.push_back(0.0f);
+            vertices.push_back(1.0f);
+            vertices.push_back(1.0f);
+            vertices.push_back(1.0f);
+
+            vertices.push_back(new_endpoint[0]);
+            vertices.push_back(new_endpoint[1]);
+            vertices.push_back(0.0f);
+            vertices.push_back(1.0f);
+            vertices.push_back(1.0f);
+            vertices.push_back(1.0f);
+
+            // Update lastEndpoint
+            last_endpoint[0] = new_endpoint[0];
+            last_endpoint[1] = new_endpoint[1];
+
+            //Update indices
+            indices.push_back(indices[indices.size()-1] + 1);
+            indices.push_back(indices[indices.size()-1] + 1);
+
         }
 
-        vertices[7] = (glfwGetTime() - new_time);
-
         // Elements object
-        Elements elements(vertices, indices, sizeof(vertices), sizeof(indices));
+        float vertices_arr[vertices.size()];
+        copy(vertices.begin(), vertices.end(), vertices_arr);
+
+        unsigned int indices_arr[indices.size()];
+        copy(indices.begin(), indices.end(), indices_arr);
+
+        Elements elements(vertices_arr, indices_arr, sizeof(vertices_arr), sizeof(indices_arr));
 
         // Writing the shaders stringsm, creating and compile the shaders programs
         string fragmentShader = shader_obj.FragShader();
@@ -75,21 +112,21 @@ int main(){
         glUseProgram(shader);
 
         // Base rendering
-        elementsmath.rotate("model", new_angle, 0.0f, 0.0f, 1.0f);
-        elementsmath.translate("view", 0.0f, -glfwGetTime() / 10.0f, 0.0f);
+        //elementsmath.rotate("model", new_angle, 0.0f, 0.0f, 1.0f);
+        elementsmath.translate("view", 0.0f, -glfwGetTime() / 5.0f, 0.0f);
         elementsmath.MountTransformMatrix();
-        elementsmath.Reset();
 
         shader_obj.UniformConfig("transform", shader, elementsmath.getTransformMatrix());
-        glDrawElements(GL_LINES, 11, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_LINES, counter, GL_UNSIGNED_INT, 0);
+        elementsmath.Reset();
 
+        cout << "new_indice_numbers:" << endl;
+        cout << counter << endl;
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
-
         //Poll for and process events
         glfwPollEvents();
-
         glFlush();
     }
 
